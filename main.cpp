2c625d6d5658db9cf600ce9904a89d8a304753de
Zhong Yun <zheng.zhi5@northeastern.edu>
2024-06-14 09:27:25 -0700

    Binding VBO, transmit data to VBO - glBindBuffer - glBufferData   - dynamic binding   - static binding

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void OnResize(int width, int height)
 }
 
 //初始化 VBO
-void prepare()
+void prepareVBO()
 {
     //创建一个 VBO，未分配显存
     GLuint VBO; //使用 GLuint 类型的变量存储 VBO 的 ID
@@ -32,6 +32,31 @@ void prepare()
 }
 
 //绑定 VBO
+void bindVBO()
+{
+    //1. 生成一个 VBO
+    //2. 绑定当前 VBO 到 openGL 状态机的当前 VBO 插槽
+    //3. 向当先 VBO 插槽写入数据 （此处以顶点坐标为例），开辟显存空间
+
+    //顶点坐标数据
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f, // 右上角
+        0.5f, -0.5f, 0.0f, // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f // 左上角
+    };
+    //1. 创建一个 VBO，未分配显存
+    GLuint VBO; //使用 GLuint 类型的变量存储 VBO 的 ID
+    GL_CALL(glGenBuffers(1, &VBO)); //生成一个 VBO, 并储存地址到 VBO 变量中
+
+    //2. 绑定到 VBO 插槽
+    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, VBO)); //GL_ARRAY_BUFFER表示当前 VBO 插槽
+
+    //3. 向 VBO 插槽写入数据开辟显存空间
+    GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW)); //GL_STATIC_DRAW表示数据不会或几乎不会改变, GL_DYNAMIC_DRAW表示数据会被改变很多
+
+    //
+}
 
 int main()
 {
@@ -45,7 +70,8 @@ int main()
     glViewport(0, 0, 800, 600);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-    prepare();
+    //prepareVBO();
+    bindVBO();
 
     //执行窗体循环
     while (app->update())
